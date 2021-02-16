a722020e41804a2df94c0d7c8d20ee8431510bfa
Juliette Gondon <juliettegondon@gmail.com>
2021-02-16 13:04:27 -0500

    routing for update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 import './style.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
