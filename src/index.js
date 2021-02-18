2c83909f7cbb7091d77d5730fc8c5b91a7d9bc45
Juliette Gondon <juliettegondon@gmail.com>
2021-02-18 17:52:31 -0500

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router} from 'react-router-dom';
-import App from './App';
-import './style.scss'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
